44928d4a03195f40f1c41d00c97a7e97fc1063d5
termux-app-store <kingmaster1143@gmail.com>
2026-02-05 20:42:23 +0700

    Update: termux-build template

--- tools/termux-build-template.sh
+++ tools/termux-build-template.sh
@@ -8,7 +8,7 @@ TERMUX_PKG_DESCRIPTION="Short description"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="Your Name <email>"
 TERMUX_PKG_VERSION=1.0.0
-TERMUX_PKG_SRCURL="https://example.com/source.tar.gz"
+TERMUX_PKG_SRCURL="https://example.com/${TERMUX_PKG_VERSION}.tar.gz"
 TERMUX_PKG_SHA256=<INPUT_SHA256_HERE>
 TERMUX_PKG_DEPENDS="python, bash, and etc"
 
