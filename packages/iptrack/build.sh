34644ab810f42afaea781855cae91baa85070e90
djunekz <gab288.gab288@passinbox.com>
2026-02-19 15:29:53 +0700

    Update packages

--- packages/iptrack/build.sh
+++ packages/iptrack/build.sh
@@ -3,7 +3,7 @@ TERMUX_PKG_DESCRIPTION="Track IP Location With Live Address And Accuracy"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="@termux-app-store"
 TERMUX_PKG_VERSION=1.0.0
-TERMUX_PKG_SRCURL=https://github.com/droidv1/termool/releases/download/v${TERMUX_PKG_VERSION}/iptrack.tar.gz
+TERMUX_PKG_SRCURL=https://github.com/djunekz/archive/releases/download/v${TERMUX_PKG_VERSION}/iptrack.tar.gz
 TERMUX_PKG_SHA256=7bbaf211e73e7e9c86eee7138abfaf9ccb9b9ed3f84f3933cb3f60db0e10156b
 TERMUX_PKG_BUILD_IN_SRC=true
 
