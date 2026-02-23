6b044c12e26d5a05717cf40003d69e7cf3615e59
djunekz <gab288.gab288@passinbox.com>
2026-02-23 18:35:29 +0700

    Update package

--- packages/metasploit/build.sh
+++
@@ -1,7 +0,0 @@
-TERMUX_PKG_HOMEPAGE=https://github.com/h4ck3r0/Metasploit-termux
-TERMUX_PKG_DESCRIPTION="Installer Latest Metasploit In Termux (2026). Features automatic Ruby 3.4+ Nokogiri/Gumbo patches, PostgreSQL auto-fix, and silent installation logic."
-TERMUX_PKG_LICENSE="GPL-3.0"
-TERMUX_PKG_MAINTAINER="@termux-app-store"
-TERMUX_PKG_VERSION=2.5.0
-TERMUX_PKG_SRCURL=https://api.github.com/repos/h4ck3r0/Metasploit-termux/tarball/${TERMUX_PKG_VERSION}
-TERMUX_PKG_SHA256=67ef935750a14c1c13b6471d6b9e1b0da69dae8cf4cfee3d1a0512881be37c07
